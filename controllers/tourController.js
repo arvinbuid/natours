946933f388b25cad6cf901ca1d45561a8e559f83
Arvin Buid <arvinbuid01@gmail.com>
2024-10-11 17:32:20 +0800

    Implement functionality of getAllTours and getTours route handlers.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,26 +1,41 @@
 const Tour = require('./../models/tourModel');
 
 // Route handlers
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success'
-    // result: tours.length,
-    // requestedAt: req.requestTime,
-    // data: {
-    //   tours // ES6 syntax to tours: tours
-    // }
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      result: tours.length,
+      data: {
+        tours // ES6 syntax to tours: tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  // const id = +req.params.id;
-  // const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success'
-    // data: {
-    //   tour
-    // }
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
