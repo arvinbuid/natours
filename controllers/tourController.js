b3219e38dd7a9d95d56000fb38081886af14351e
Arvin Buid <arvinbuid01@gmail.com>
2024-10-11 17:17:17 +0800

    Create models folder with tourModel file to store the tourSchema.
    Add engines property inside package.json file to support async operations.
    
    Implement createTour functionality to create new tour and save into mongodb.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,76 +1,45 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-// Middlewares
-exports.checkID = (req, res, next, val) => {
-  // console.log(`The id is: ${val}`);
-
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid body, please provide a name or price.'
-    });
-  }
-  next();
-};
+const Tour = require('./../models/tourModel');
 
 // Route handlers
 exports.getAllTours = (req, res) => {
   res.status(200).json({
-    status: 'success',
-    result: tours.length,
-    requestedAt: req.requestTime,
-    data: {
-      tours // ES6 syntax to tours: tours
-    }
+    status: 'success'
+    // result: tours.length,
+    // requestedAt: req.requestTime,
+    // data: {
+    //   tours // ES6 syntax to tours: tours
+    // }
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = +req.params.id;
-  const tour = tours.find(el => el.id === id);
-
+  // const id = +req.params.id;
+  // const tour = tours.find(el => el.id === id);
   res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
+    status: 'success'
+    // data: {
+    //   tour
+    // }
   });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
 
-  tours.push(newTour);
-
-  // read tours-simple file and save the newly created tour
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    () => {
-      res.status(201).json({
-        status: 'success',
-        message: 'New tour created',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
+    res.status(200).json({
+      status: 'success',
+      message: 'New tour created successfully',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
@@ -88,3 +57,23 @@ exports.deleteTour = (req, res) => {
     data: null
   });
 };
+
+////////////////////////////////
+//// FOR FUTURE REFERENCES
+
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
+
+// Middlewares
+// exports.checkID = (req, res, next, val) => {
+//   // console.log(`The id is: ${val}`);
+
+//   if (+req.params.id > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID'
+//     });
+//   }
+//   next();
+// }
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-// DB configuration
+// DB connection
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -21,37 +21,7 @@ mongoose
     console.log('Database connection successful.');
   });
 
-// Schema
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'Tour must have a price']
-  }
-});
-
-// Model
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 299
-});
-
-testTour
-  .save()
-  .then(doc => console.log(doc))
-  .catch(err => console.log('ERROR💥 : ', err));
-
+// Port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
