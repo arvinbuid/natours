59ae7cc1420de6919023f3f383377a14e631ca95
Arvin Buid <arvinbuid01@gmail.com>
2024-10-12 21:42:25 +0800

    Create import-dev-data.js script file to quickly delete and import a test data into mongoDB from reading tours-simple.json file.

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// DB connection
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Database connection successful.');
+  });
+
+// READ THE JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data imported successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data deleted successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// RUN IN COMMAND LINE TO IMPORT || DELETE ALL DATA TO/FROM DB
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+// CLI SCRIPTS FOR IMPORT/DELETE DATA
+// node dev-data/data/import-dev-data.js --import
+// node dev-data/data/import-dev-data.js --delete
