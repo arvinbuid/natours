bb5ae66cbbe8aa981a2f243f3809c249fa84f233
Arvin Buid <arvinbuid01@gmail.com>
2024-10-14 14:32:07 +0800

    Implement pagination to GET all tours.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,7 +24,7 @@ exports.getAllTours = async (req, res) => {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt');
+      query = query.sort('_id');
     }
 
     // 3.) FIELD LIMITING
@@ -33,6 +33,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select(fields);
     } else {
       query = query.select('-__v');
+    }
+
+    // 4.) PAGINATION
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist.');
     }
 
     // EXECUTE QUERY
