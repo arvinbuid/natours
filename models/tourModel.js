677cbc89148549d0fa58f25de8676d51761b6895
Arvin Buid <arvinbuid01@gmail.com>
2024-10-15 20:41:49 +0800

    Add a 'pre' document middleware and install slugify package to learn mongoose middleware.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'Tour must have a duration']
@@ -64,7 +66,26 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+// DOCUMENT MIDDLEWARE - runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document...💿💿');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
 // Model
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
+
+// 4 TYPES OF MIDDLEWARE IN MONGOOSE:
+// DOCUMENT, QUERY, AGGREGATE, MODEL MIDDLEWARE
