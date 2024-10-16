aa2aa62ec2e2d9c28787463a3e52bd0071b5816d
Arvin Buid <arvinbuid01@gmail.com>
2024-10-16 19:35:36 +0800

    Implement global error handling middleware.

--- app.js
+++ app.js
@@ -21,11 +21,24 @@ app.use('/api/v1/tours', tourRouter); // mounting the router
 app.use('/api/v1/users', userRouter);
 
 app.all('*', function(req, res, next) {
-  res.status(404).json({
-    status: 'fail',
-    message: `The requested ${req.originalUrl} cannot be found😔`
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `The requested ${req.originalUrl} cannot be found😔`
+  // });
+  const err = new Error(`The requested ${req.originalUrl} cannot be found😔`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
-  next();
 });
 
 module.exports = app;
