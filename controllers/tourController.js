0d7dd7f1395784f35efe4c1057a286d8e8c3470a
Arvin Buid <arvinbuid01@gmail.com>
2024-10-17 16:55:08 +0800

    Add a 404 not found error in getTour, updateTour, and deleteTour route handler.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
-const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const APIFeatures = require('./../utils/apiFeatures');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -32,6 +33,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
 
+  if (tour === null) {
+    return next(new AppError('Cannot fint tour with that id.', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -58,6 +63,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true
   });
 
+  if (tour === null) {
+    return next(new AppError('Cannot fint tour with that id.', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -67,7 +76,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (tour === null) {
+    return next(new AppError('Cannot fint tour with that id.', 404));
+  }
 
   res.status(204).json({
     status: 'success',
