78023663f92c188696962522ad8af410970b95ee
Arvin Buid <arvinbuid01@gmail.com>
2024-10-23 09:56:18 +0800

    Replace embedding user into tours document and instead reference User and Tours model.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./../models/userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -112,7 +111,12 @@ const tourSchema = new mongoose.Schema(
         description: String
       }
     ],
-    guides: Array
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -130,11 +134,11 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-tourSchema.pre('save', async function(next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 // QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function(next) {
