2afd2c63e56e62059dbeb800a1e9b65146dcf498
Arvin Buid <arvinbuid01@gmail.com>
2024-10-27 16:10:28 +0800

    Create overview and tour views and extend the base view.

--- app.js
+++ app.js
@@ -73,10 +73,23 @@ app.use(
 // });
 
 // ROUTES
+// Views
 app.get('/', (req, res) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
     user: 'Arvin'
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
   });
 });
 
