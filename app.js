727e8dd3b9c664777415c702b8a12774bcbefea2
Arvin Buid <arvinbuid01@gmail.com>
2024-10-28 20:21:51 +0800

    Install cookie-parser, create userLogin.js file, add axios cdn and implement route protection using cookies coming from the request.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const deepSanitize = require('./utils/deepSanitize');
@@ -45,6 +46,9 @@ app.use('/api', limiter);
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+// Cookie parser
+app.use(cookieParser());
+
 // Data sanitization against NOSQL query injection
 app.use(mongoSanitize());
 
@@ -69,9 +73,10 @@ app.use(
 );
 
 // Other middlewares
-// app.use((req, res, next) => {
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log(req.cookies);
+  next();
+});
 
 // ROUTES
 app.use('/', viewRouter);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const viewController = require('./../controllers/viewsController');
+// const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
