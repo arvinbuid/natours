80fc5cfda9648e35d19d7c16703847b183f193cf
Arvin Buid <arvinbuid01@gmail.com>
2024-10-28 18:41:10 +0800

    Create login view, login route and create getLoginForm route handler.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1.) Get tour data from collection
   const tours = await Tour.find();
 
@@ -13,7 +13,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1.) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -27,3 +27,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = async (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log in to your account'
+  });
+};
